c82131e07f8e8345504196c40dab5088ae4f92fd
riveranb <riveranb@gmail.com>
2020-10-24 21:17:35 +0800

    collect series of template pictures for pyautogui.locateOnScreen()

--- getudemy.py
+++ getudemy.py
@@ -1,15 +1,16 @@
 # 2020/10/24
 # Uses pyautogui to detect feature images and handling click operations
 
+import os
 import pyautogui
 
 def locate_rectangle(picpathlist, conf=0.8):
     pyautogui.PAUSE = 1
 
-    for path in picpathlist:
-        tryCount = 5
-        while tryCount >= 0:
-            tryCount -= 1
+    tryCount = 5 # times to try
+    while tryCount >= 0:
+        tryCount -= 1
+        for path in picpathlist: # for each picture-template
             # find register button
             registerbtn = pyautogui.locateOnScreen(path, confidence=conf)
             if registerbtn == None:
@@ -18,11 +19,25 @@ def locate_rectangle(picpathlist, conf=0.8):
             if registerbtn != None:
                 return registerbtn
 
+def collect_pictureseries(templatename):
+    postfixes = ['_small_cht_chrome.png', '_cht_chrome.png', '_small_chs_chrome.png', '_chs_chrome.png',
+                 '_small_cht_firefox.png', '_cht_firefox.png', '_small_chs_firefox.png', '_chs_firefox.png']
+    
+    series = []
+    for postf in postfixes:
+        filename = str(templatename) + postf
+        if os.path.isfile(filename):
+            series.append(filename)
+    
+    return series
+
 def register_udemycourse():
     pyautogui.PAUSE = 10
     
     print ("Detect Udemy logo: ", pyautogui.locateOnScreen('pics\\udemylogo.png', confidence=0.8))
-    startRect = locate_rectangle(['pics\\gotocoursegreenbg_cht.png', 'pics\\startlearnredbg_cht.png'])
+    picfiles = collect_pictureseries('pics\\gotocoursegreenbg')
+    picfiles += collect_pictureseries('pics\\startlearnredbg')
+    startRect = locate_rectangle(picfiles)
     if startRect != None:
         pyautogui.PAUSE = 1
         pyautogui.moveTo(startRect, duration=0.5)
@@ -30,12 +45,14 @@ def register_udemycourse():
         return
 
     # confidence = 0.8, not works
-    freeRect = locate_rectangle(['pics\\free_cht.png', 'pics\\free_chs.png', 'pics\\free_small_cht.png'], 0.7)
+    picfiles = collect_pictureseries('pics\\free')
+    freeRect = locate_rectangle(picfiles, 0.7)
     if freeRect == None:
         print("Not a free course.")
         return
 
-    registerbtnRect = locate_rectangle(['pics\\registernow_cht.png', 'pics\\registernow_chs.png', 'pics\\registernow_small_cht.png'])
+    picfiles = collect_pictureseries('pics\\registernow')
+    registerbtnRect = locate_rectangle(picfiles)
     if registerbtnRect == None:
         print ("Not found register button, give it up.")
         return
@@ -45,10 +62,12 @@ def register_udemycourse():
     pyautogui.click(registerbtnRect) # 1st click register
 
     # try looking for start course button
-    startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'])
+    picfiles = collect_pictureseries('pics\\startlearnredbg')
+    startRect = locate_rectangle(picfiles)
     if startRect == None:
         # search for red-background register button again
-        registerbtnRect = locate_rectangle(['pics\\registernowredbg_cht.png'])
+        picfiles = collect_pictureseries('pics\\registernowredbg')
+        registerbtnRect = locate_rectangle(picfiles)
         if registerbtnRect == None:
             print ("Not found register button to continue, give it up.")
             return
@@ -56,7 +75,8 @@ def register_udemycourse():
         pyautogui.moveTo(registerbtnRect, duration=0.5)
         pyautogui.PAUSE = 10
         pyautogui.click(registerbtnRect) # 2nd click register
-        startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'])
+        picfiles = collect_pictureseries('pics\\startlearnredbg')
+        startRect = locate_rectangle(picfiles)
     
     if startRect == None:
         print ("Not found start course button, give it up.")
