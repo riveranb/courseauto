b089b76ee7133b86fa907fa163c7caa7c9964def
riveranb <riveranb@gmail.com>
2020-10-24 19:57:53 +0800

    webbrowser register 'FireFox'; getudemy locate multiple picture-templates at a time

--- getudemy.py
+++ getudemy.py
@@ -3,34 +3,37 @@
 
 import pyautogui
 
-def locate_rectangle(picpath, conf=0.8):
+def locate_rectangle(picpathlist, conf=0.8):
     pyautogui.PAUSE = 1
-    tryCount = 5
-    while tryCount >= 0:
-        tryCount -= 1
-        # find register button
-        registerbtn = pyautogui.locateOnScreen(picpath, confidence=conf)
-        if registerbtn == None:
-            # try simplified chinese case
-            registerbtn = pyautogui.locateOnScreen(picpath, confidence=conf)
-        if registerbtn != None:
-            return registerbtn
+
+    for path in picpathlist:
+        tryCount = 5
+        while tryCount >= 0:
+            tryCount -= 1
+            # find register button
+            registerbtn = pyautogui.locateOnScreen(path, confidence=conf)
+            if registerbtn == None:
+                # try simplified chinese case
+                registerbtn = pyautogui.locateOnScreen(path, confidence=conf)
+            if registerbtn != None:
+                return registerbtn
 
 def register_udemycourse():
     pyautogui.PAUSE = 10
     
     print ("Detect Udemy logo: ", pyautogui.locateOnScreen('pics\\udemylogo.png', confidence=0.8))
-    startRect = pyautogui.locateOnScreen('pics\\gotocoursegreenbg_cht.png', confidence=0.8)
+    startRect = locate_rectangle(['pics\\gotocoursegreenbg_cht.png', 'pics\\startlearnredbg_cht'], 0.45)
     if startRect != None:
         print ("Already registered.")
         return
 
-    freeRect = locate_rectangle('pics\\free_cht.png', 0.7)
+    freeRect = locate_rectangle(['pics\\free_cht.png', 'pics\\free_chs.png'], 0.45)
     if freeRect == None:
         print("Not a free course.")
         return
 
-    registerbtnRect = locate_rectangle('pics\\registernow_cht.png')
+    registerbtnRect = locate_rectangle(['pics\\registernow_cht.png', 'pics\\registernow_chs.png', 'pics\\registernow_small_cht.png'], 
+                                        0.45)
     if registerbtnRect == None:
         print ("Not found register button, give it up.")
         return
@@ -40,10 +43,10 @@ def register_udemycourse():
     pyautogui.click(registerbtnRect) # 1st click register
 
     # try looking for start course button
-    startRect = locate_rectangle('pics\\startlearnredbg_cht.png', 0.8)
+    startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'], 0.45)
     if startRect == None:
         # search for red-background register button again
-        registerbtnRect = locate_rectangle('pics\\registernowredbg_cht.png')
+        registerbtnRect = locate_rectangle(['pics\\registernowredbg_cht.png'], 0.45)
         if registerbtnRect == None:
             print ("Not found register button to continue, give it up.")
             return
@@ -51,7 +54,7 @@ def register_udemycourse():
         pyautogui.moveTo(registerbtnRect, duration=0.5)
         pyautogui.PAUSE = 10
         pyautogui.click(registerbtnRect) # 2nd click register
-        startRect = locate_rectangle('pics\\startlearnredbg_cht.png', 0.8)
+        startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'], 0.45)
     
     if startRect == None:
         print ("Not found start course button, give it up.")
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+import sys
 import webbrowser
 import time
 import ctypes  # An included library with Python install
 
-import getudemy as getud
+import getudemy
+
+# parse argument
+if len(sys.argv) < 2:
+    print ("Usage: py main.py [urls-filepath]")
+    exit(1)
+
+print ("Input file: ", sys.argv[1])
+
+urlfile = sys.argv[1]
 
 # register Google Chrome browser (for Windows)
-chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+chrome_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+firefox_path = 'D:\\Program Files\\Mozilla Firefox\\firefox.exe'
+webbrowser.register('firefox', None, webbrowser.BackgroundBrowser(firefox_path))
 
-url_lines = open('urls.txt', 'r').readlines()
+url_lines = open(urlfile, 'r').readlines()
 for url in url_lines:
-    webbrowser.get("chrome").open_new_tab(url)
+    webbrowser.get('chrome').open_new_tab(url)
     # wait for browser
     time.sleep(10)
-    getud.register_udemycourse()
+    getudemy.register_udemycourse()
     print("Done: " + url)
 
 ctypes.windll.user32.MessageBoxW(0, "courseauto complete!", "Message", 0)
