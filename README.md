a073518a8101829ad88c9fc279d5ddc990929265
riveranb <riveranb@gmail.com>
2020-10-24 20:23:34 +0800

    pyautogui.locateOnScreen( ... , confidence=0.45) not works, too low;

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ This is just my own practice.
 Currently works for Udemy. You need to log-in automatically with browser.
 Currently supports language: 繁體中文
 
+### Usage
+py main.py [url-list-filepath]
+
 ### Prerequisite
 * pip install pyautogui
 * pip install opencv_python
--- getudemy.py
+++ getudemy.py
@@ -22,18 +22,20 @@ def register_udemycourse():
     pyautogui.PAUSE = 10
     
     print ("Detect Udemy logo: ", pyautogui.locateOnScreen('pics\\udemylogo.png', confidence=0.8))
-    startRect = locate_rectangle(['pics\\gotocoursegreenbg_cht.png', 'pics\\startlearnredbg_cht'], 0.45)
+    startRect = locate_rectangle(['pics\\gotocoursegreenbg_cht.png', 'pics\\startlearnredbg_cht.png'])
     if startRect != None:
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(startRect, duration=0.5)
         print ("Already registered.")
         return
 
-    freeRect = locate_rectangle(['pics\\free_cht.png', 'pics\\free_chs.png'], 0.45)
+    # confidence = 0.8, not works
+    freeRect = locate_rectangle(['pics\\free_cht.png', 'pics\\free_chs.png', 'pics\\free_small_cht.png'], 0.7)
     if freeRect == None:
         print("Not a free course.")
         return
 
-    registerbtnRect = locate_rectangle(['pics\\registernow_cht.png', 'pics\\registernow_chs.png', 'pics\\registernow_small_cht.png'], 
-                                        0.45)
+    registerbtnRect = locate_rectangle(['pics\\registernow_cht.png', 'pics\\registernow_chs.png', 'pics\\registernow_small_cht.png'])
     if registerbtnRect == None:
         print ("Not found register button, give it up.")
         return
@@ -43,10 +45,10 @@ def register_udemycourse():
     pyautogui.click(registerbtnRect) # 1st click register
 
     # try looking for start course button
-    startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'], 0.45)
+    startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'])
     if startRect == None:
         # search for red-background register button again
-        registerbtnRect = locate_rectangle(['pics\\registernowredbg_cht.png'], 0.45)
+        registerbtnRect = locate_rectangle(['pics\\registernowredbg_cht.png'])
         if registerbtnRect == None:
             print ("Not found register button to continue, give it up.")
             return
@@ -54,7 +56,7 @@ def register_udemycourse():
         pyautogui.moveTo(registerbtnRect, duration=0.5)
         pyautogui.PAUSE = 10
         pyautogui.click(registerbtnRect) # 2nd click register
-        startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'], 0.45)
+        startRect = locate_rectangle(['pics\\startlearnredbg_cht.png'])
     
     if startRect == None:
         print ("Not found start course button, give it up.")
